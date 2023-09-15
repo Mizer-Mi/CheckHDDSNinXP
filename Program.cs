dcf3e5ea0844bfa017015e807faefc20f5b02611
Mizer-Mi <mucahitizer@gmail.com>
2023-09-15 13:37:14 +0300

    Project Files
    Project Files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using IOEx;
+
+namespace CheckHDDSNinXP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                DriveListEx dd = new DriveListEx();
+                dd.Load();
+                foreach (var drive in dd)
+                {
+                    if (drive.SerialNumber.Trim() == "WD-WCC6Y0TPNTK2")
+                    {
+                        Console.WriteLine("HDD NO Correct - HDD SN Dogru.");
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("HDD SN incorrect - HDD SN Dogru degil.");
+            Console.ReadKey();
+        }
+    }
+}
